3a89d65bcff5751642cc03dde626ffa77287c8d7
Patrick Hurst <phurst@mit.edu>
2010-10-19 16:24:13 -0400

    add publicTimeline, refactor code for ease of adding methods

--- Web/Twitter.hs
+++ Web/Twitter.hs
@@ -1,18 +1,27 @@
-module Web.Twitter (
-    updateStatus
-    ) where
+module Web.Twitter
+       ( updateStatus
+       , publicTimeline
+       ) where
 
 import Data.Maybe (fromJust)
 import Web.Twitter.OAuth
-import Control.Monad.Trans (liftIO)
+import Control.Monad.Trans (liftIO, MonadIO)
 import Network.OAuth.Consumer
 import Network.OAuth.Http.Request
 import Network.OAuth.Http.Response
 import Network.OAuth.Http.HttpClient
 
+buildRequest ::  Method -> String -> [(String, String)] -> Request
+buildRequest method part query =
+    (fromJust . parseURL $ "http://api.twitter.com/1/" ++ part ++ ".json") { method = method, qString = fromList query}
+
+doRequest :: (MonadIO m, HttpClient m) => Method -> String -> [(String, String)] -> OAuthMonad m Response
+doRequest meth part query = serviceRequest HMACSHA1 Nothing $ buildRequest meth part query
+
 updateStatus :: Token -> String -> IO Response
 updateStatus token status = unwrap $ do
     putToken token
-    let request = url { method = POST, qString = fromList [("status", status)]}
-    serviceRequest HMACSHA1 Nothing request
-    where url = fromJust . parseURL $ "http://api.twitter.com/1/statuses/update.json"
+    doRequest POST "statuses/update"  [("status", status)]
+
+publicTimeline :: IO Response
+publicTimeline  = unwrap $ doRequest GET "statuses/public_timeline" []
